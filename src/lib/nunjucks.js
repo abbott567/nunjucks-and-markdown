0f94d9186d0bde4c396b9038a91fc5c52de1153a
Craig Abbott <craigabbott.digitaldwp@gmail.com>
2021-08-17 19:23:16 +0100

    Use WCAGify Nunjucks Macro

--- src/lib/nunjucks.js
+++ src/lib/nunjucks.js
@@ -3,6 +3,7 @@ const fileHelper = require('./file-helper')
 const generateTOC = require('./toc-generator')
 const markdown = require('./markdown')
 const path = require('path')
+const wcagify = require('wcagify')
 
 function setup (app) {
   app.set('view engine', 'njk')
@@ -11,7 +12,8 @@ function setup (app) {
     path.join(__dirname, '../', '../', 'markdown'),
     path.join(__dirname, '../', '../', 'src'),
     path.join(__dirname, '../', '../', 'src', 'views', 'components'),
-    path.join(__dirname, '../', '../', 'src', 'views', 'layouts')
+    path.join(__dirname, '../', '../', 'src', 'views', 'layouts'),
+    path.join(__dirname, '../', '../', 'node_modules', 'wcagify', 'nunjucks')
   ]
 
   const nunjucksEnvironment = nunjucks.configure(paths, {
@@ -23,6 +25,7 @@ function setup (app) {
 
   nunjucksEnvironment.addFilter('markdown', markdown.compile)
   nunjucksEnvironment.addFilter('toc', generateTOC)
+  nunjucksEnvironment.addFilter('wcagify', wcagify)
   nunjucksEnvironment.addGlobal('getHtmlCode', fileHelper.getHtmlCode)
   nunjucksEnvironment.addGlobal('getNunjucksCode', fileHelper.getNunjucksCode)
 
